b4da04645754c2e676356dd6974632edac2b3af7
keneishac <keneishacorbett@gmail.com>
2021-01-22 09:19:55 -0800

    Adds firebase setup

---
+++ src/services/firebase.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCbwJFjzruaBY0BpjxYaOGND0U3HNkKx04",
+    authDomain: "starwarsify-882bb.firebaseapp.com",
+    projectId: "starwarsify-882bb",
+    storageBucket: "starwarsify-882bb.appspot.com",
+    messagingSenderId: "556418576250",
+    appId: "1:556418576250:web:6f6edf737e1cd8708170c3"
+  };
