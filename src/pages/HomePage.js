b9d7b4b996bcead90aeb5ea1d555a1d4b01ce86b
keneishac <keneishacorbett@gmail.com>
2021-01-22 09:00:13 -0800

    styles  diplay on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,15 +5,26 @@ const StyledPage = styled.main`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-wrap: wrap;
+    @media (max-width: 1150px){
+        flex-direction: column;
+    }
 `;
 
 const StyledStarshipcard = styled.article`
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
     margin: 15px;
-    flex-grow: 1;
     border: 1px solid #000000;
     border-radius: 12px;
-`;
+    height: 250px;
+    width: 350px;
+    h1{
+        text-align: center;
 
+    }
+`;
 
 export default function HomePage(props){
     return (
@@ -22,6 +33,10 @@ export default function HomePage(props){
                 props.sampleStarships.map(starship =>
                     <StyledStarshipcard key={starship.name}>
                         <h1>{starship.name}</h1>
+                        <p><strong>Manufacturer:</strong> {starship.manufacturer}</p>
+                        <p><strong>Passenger Capacity:</strong> {starship.passengers}</p>
+                        <p><strong>Hyperdrive Rating:</strong> {starship.hyperdrive_rating}</p>
+
                     </StyledStarshipcard>
                     )
             }
